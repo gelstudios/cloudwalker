530bbff99ceeaa792539f70c700453b9a402adce
Eric Romano <gelstudios@gmail.com>
2019-02-19 15:20:49 -0500

    blink builtin if wifi is not connected, blink builtin on successful request

--- firmware/cloudwalker/cloudwalker.ino
+++ firmware/cloudwalker/cloudwalker.ino
@@ -1,10 +1,10 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-uint64_t chipid;  
+uint64_t chipid;
 
 // pin ID setup
-int Bpin = 15; 
+int Bpin = 15;
 int Gpin = 32;
 int Rpin = 14;
 int VBAT = A13;
@@ -33,12 +33,22 @@ const char* password = "iloveiot";
 // HTTP polling delay
 int pollingDelay = 100;
 
-// wifi + http client 
+// wifi + http client
 WiFiClient wifi;
 HTTPClient http;
 
+const int rainbow[7][3] = {
+  {255, 000, 000}, //r
+  {000, 000, 000}, //o
+  {000, 000, 000}, //y
+  {000, 255, 000}, //g
+  {000, 000, 255}, //b
+  {000, 000, 000}, //i 
+  {000, 000, 000}, //v
+};
+
 void setup() {
-	Serial.begin(115200);
+  Serial.begin(115200);
   pinMode(Rpin, OUTPUT); // R
   pinMode(Gpin, OUTPUT); // G
   pinMode(Bpin, OUTPUT); // B
@@ -64,6 +74,10 @@ void setupWifi() {
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
+    for (int i = 0; i <= 1; i++) {
+      blinkStatusLed();
+      delay(100);
+    }
     delay(500);
     Serial.print(".");
   }
@@ -76,64 +90,63 @@ void setupWifi() {
 
 void loop() {
   getColorFromCloud();
-  
-  blinkStatusLed();
 
   fadeColor();
   // if you dont like fade, use update :)
   // updateColor();
- 
+
   Serial.println(analogRead(VBAT) * 2);
 
   delay(pollingDelay);
 }
 
-void getColorFromCloud(){
+void getColorFromCloud() {
   http.begin("http://api.iot.shoes/checkin/1");
   http.setReuse(true);
   http.setTimeout(500);
 
   int httpStatusCode = http.GET();
   if (httpStatusCode > 0) {
-        String payload = http.getString();
-        Serial.print(httpStatusCode);
-        Serial.println(payload);
+    blinkStatusLed();
+    String payload = http.getString();
+    Serial.print(httpStatusCode);
+    Serial.println(payload);
 
-        int requestR, requestG, requestB;
+    int requestR, requestG, requestB;
 
-        if (sscanf(payload.c_str(), "%d,%d,%d", &requestR, &requestG, &requestB) == 3) {
-          if ( requestR >= 0 && requestR <= 256) {
-            if ( requestG >= 0 && requestG <= 256) {
-              if ( requestB >= 0 && requestB <= 256) {
-                 r = requestR;
-                 g = requestG;
-                 b = requestB;
-              }
-            }
+    if (sscanf(payload.c_str(), "%d,%d,%d", &requestR, &requestG, &requestB) == 3) {
+      if (requestR >= 0 && requestR <= 256) {
+        if (requestG >= 0 && requestG <= 256) {
+          if (requestB >= 0 && requestB <= 256) {
+            r = requestR;
+            g = requestG;
+            b = requestB;
           }
         }
+      }
+    }
   } else {
     http.end(); // free http client handle
     Serial.print("Error on HTTP request");
-    Serial.print(":");
+    Serial.print(": ");
     Serial.println(httpStatusCode);
   }
 }
 
-void blinkStatusLed(){
+void blinkStatusLed() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(50);
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-void updateColor(){
+void updateColor() {
   ledcWrite(Rchannel, r);
   ledcWrite(Gchannel, g);
   ledcWrite(Bchannel, b);
 }
 
-void fadeColor(){
-  while (r != currentR || g != currentG || b != currentB){
+void fadeColor() {
+  while (r != currentR || g != currentG || b != currentB) {
     Serial.println("step 2");
     stepTo(Rchannel, currentR, r);
     stepTo(Gchannel, currentG, g);
@@ -141,14 +154,14 @@ void fadeColor(){
   }
 }
 
-void stepTo(int CHAN, int &current, int target){
+void stepTo(int CHAN, int &current, int target) {
   int v = 0;
 
-  if ( current > target ){
-     v = -1;
+  if (current > target) {
+    v = -1;
   }
-  if ( current < target ){
-     v = +1;
+  if (current < target) {
+    v = +1;
   }
   ledcWrite(CHAN, current + v);
   current += v;
